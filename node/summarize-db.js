4346b3ed25241546fa52533d718faa3bf6d327bf
vipulroxx <sharm493@morris.umn.edu>
2017-10-09 16:48:35 -0500

    Attempt to fix the out of order issue

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -6,6 +6,7 @@ var util  = require("util");
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
 var connectionClosed = false;
+var printingDatabase = "";
 
 var printDatabaseSummary = function (databaseName) {
     var query = "SHOW tables IN " + databaseName + ";";
@@ -14,21 +15,25 @@ var printDatabaseSummary = function (databaseName) {
             console.log("error in printDatabaseSummary" + err);
             console.log("query was: " + query);
         } else {
-            console.log("---|" + databaseName + ">");
+            //console.log("---|" + databaseName + ">");
             rows.map(function(row){
-              printTableSummary(databaseName + "." + row["Tables_in_" + databaseName]);
+              printTableSummary(databaseName + "." + row["Tables_in_" + databaseName], databaseName);
             });
        } 
    })
 };
 
-var printTableSummary = function (tableName) {
+var printTableSummary = function (tableName, databaseName) {
     var tableQuery = "Describe " + tableName;
     connection.query(tableQuery, function(err,rows,fields) {
 	if (err) {
 	    console.log("error in printTableSummary" +err);
 	}
 	else {
+           if (databaseName !== printingDatabase) {
+              console.log("---|" + databaseName + ">");
+              printingDatabase = databaseName;
+           }
 	   console.log('......|' + tableName + ">");
 	   rows.map(function(field){
 		   var entry = "        FieldName: "
@@ -59,7 +64,7 @@ connection.query('SHOW DATABASES',function(err,rows,fields){
         console.log('Error looking up databases');
     } else {
         rows.map(function(row){
-            return row['Database'];
-        }).map(printDatabaseSummary);
+            printDatabaseSummary(row['Database']);
+        });
     }
 });

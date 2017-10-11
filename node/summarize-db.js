45ec9524f7eef96ad879c2a5962e88826b2e1f00
Laverne Schrock <schr1230@morris.umn.edu>
2017-10-11 16:52:35 -0500

    Remove unused vars and comments

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -5,8 +5,6 @@ var util  = require("util");
 
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
-var connectionClosed = false;
-var printingDatabase = "";
 var collectedData = {};
 
 var printDatabaseSummary = function (databaseName) {
@@ -33,18 +31,11 @@ var printTableSummary = function (tableName, databaseName) {
 	    console.log("error in printTableSummary" +err);
 	}
 	else {
-//           if (databaseName !== printingDatabase) {
-//              console.log("---|" + databaseName + ">");
-//              printingDatabase = databaseName;
-//           }
-//	   console.log('......|' + tableName + ">");
 	   rows.map(function(field){
 		   var entry = "        FieldName: "
 		               + "`" + field.Field + "`"
 		               + "\t(" + field.Type + ")"; 
                    collectedData.databases[databaseName].tables[tableName].push(entry);
-		   //console.log(entry);
-		   //console.log(field);
 	   });
 
            collectedData.databases[databaseName].tablesLeftToProcess -= 1;
@@ -54,14 +45,11 @@ var printTableSummary = function (tableName, databaseName) {
 }
 
 function printCollectedData() {
-    //collectedData.databases.map(function(x){console.log(util.inspect(x),{depth:1})});
 
     for (var databaseName in collectedData.databases) {
-        //console.log("key: " + key + ",\n" + "value: " + util.inspect(collectedData.databases[key], {depth: 0}));
 
         if (collectedData.databases[databaseName].tablesLeftToProcess === 0) {
           console.log("---|" + databaseName + ">");
-          //console.log(collectedData.databases[key].tables);
           for (var tableName in collectedData.databases[databaseName].tables) {
               console.log('......|' + tableName + ">");
               console.log(collectedData.databases[databaseName].tables[tableName].join("\n"));

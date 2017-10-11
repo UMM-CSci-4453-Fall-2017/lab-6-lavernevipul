8f21cbb3358de9cad7811e30c3038aad4f64c196
Laverne Schrock <schr1230@morris.umn.edu>
2017-10-11 16:39:11 -0500

    Enforce correct order of printing even when the DB is slow
    This new code is really sloppy and needs to be cleaned up quite a bit.

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -7,6 +7,7 @@ credentials.host="ids";
 var connection = mysql.createConnection(credentials);
 var connectionClosed = false;
 var printingDatabase = "";
+var collectedData = {};
 
 var printDatabaseSummary = function (databaseName) {
     var query = "SHOW tables IN " + databaseName + ";";
@@ -15,8 +16,10 @@ var printDatabaseSummary = function (databaseName) {
             console.log("error in printDatabaseSummary" + err);
             console.log("query was: " + query);
         } else {
-            //console.log("---|" + databaseName + ">");
+            collectedData.databases[databaseName].tablesLeftToProcess = rows.length;
+            collectedData.databases[databaseName].tables = {};
             rows.map(function(row){
+              collectedData.databases[databaseName].tables[databaseName + "." + row["Tables_in_" + databaseName]] = [];
               printTableSummary(databaseName + "." + row["Tables_in_" + databaseName], databaseName);
             });
        } 
@@ -30,24 +33,47 @@ var printTableSummary = function (tableName, databaseName) {
 	    console.log("error in printTableSummary" +err);
 	}
 	else {
-           if (databaseName !== printingDatabase) {
-              console.log("---|" + databaseName + ">");
-              printingDatabase = databaseName;
-           }
-	   console.log('......|' + tableName + ">");
+//           if (databaseName !== printingDatabase) {
+//              console.log("---|" + databaseName + ">");
+//              printingDatabase = databaseName;
+//           }
+//	   console.log('......|' + tableName + ">");
 	   rows.map(function(field){
 		   var entry = "        FieldName: "
 		               + "`" + field.Field + "`"
 		               + "\t(" + field.Type + ")"; 
-		   console.log(entry);
+                   collectedData.databases[databaseName].tables[tableName].push(entry);
+		   //console.log(entry);
 		   //console.log(field);
 	   });
+
+           collectedData.databases[databaseName].tablesLeftToProcess -= 1;
+           printCollectedData();
+           //console.log(util.inspect(collectedData, {depth:5}));
 	   if (!connectionClosed) {
 		connectionClosed = true;
 		connection.end();
 	   }
 	}
     });
+}
+
+function printCollectedData() {
+    //collectedData.databases.map(function(x){console.log(util.inspect(x),{depth:1})});
+
+    for (var databaseName in collectedData.databases) {
+        //console.log("key: " + key + ",\n" + "value: " + util.inspect(collectedData.databases[key], {depth: 0}));
+
+        if (collectedData.databases[databaseName].tablesLeftToProcess === 0) {
+          console.log("---|" + databaseName + ">");
+          //console.log(collectedData.databases[key].tables);
+          for (var tableName in collectedData.databases[databaseName].tables) {
+              console.log('......|' + tableName + ">");
+              console.log(collectedData.databases[databaseName].tables[tableName].join("\n"));
+          }
+          delete collectedData.databases[databaseName];
+        }
+    }
 }
 
 connection.connect(function(err){
@@ -63,7 +89,9 @@ connection.query('SHOW DATABASES',function(err,rows,fields){
     if(err){
         console.log('Error looking up databases');
     } else {
+        collectedData.databases = {};
         rows.map(function(row){
+            collectedData.databases[row['Database']] = {};
             printDatabaseSummary(row['Database']);
         });
     }
